6c1af61c71a995ef8826c171c3380aa99b62dee1
Edixon Hernandez <edixon.hernandez.c@gmail.com>
2016-07-19 23:03:53 -0500

    Peces en el lago y sonidos del rio

--- Assets/Script/Lago.cs
+++ Assets/Script/Lago.cs
@@ -3,23 +3,13 @@ using System.Collections;
 
 public class Lago : MonoBehaviour {
 
-	float speedx = 10,speedy = 5,speedz = -10, tiempo = 1;
+	float speedx = 0,speedy = 0,speedz = 0, tiempo = 1;
 	public GameObject pez;
 
 	// Use this for initialization
 	void Start () {
-		speedx = Random.Range (-5f, 5f);
-		speedy = Random.Range (-1.5f, 1.5f);
-		transform.Translate (0 , Random.Range(-2,2), Random.Range(-5,5));
-
-	}
-
-	void OnNetworkLoadedLevel()
-	{
-		if(Network.isServer){
-			Network.Instantiate (pez,transform.position,transform.rotation,0);
-			Network.Instantiate (pez,transform.position,transform.rotation,0);
-		}
+		for(int i = 0; i < 2;i++)
+			Instantiate (pez,transform.position,transform.rotation);
 	}
 
 	// Update is called once per frame
@@ -33,16 +23,20 @@ public class Lago : MonoBehaviour {
 				speedz = 10;
 			}
 			tiempo = 1;
+
+			if(speedx == 10)
+			{	
+				speedx = -10;
+			}else{
+				speedx = 10;
+			}
+			
+			speedy -= 0.1f;
+			if(speedy < -1f)
+				speedy = 1f;
 		}
-		speedx -= 0.1f;
-		if(speedx < -10f)
-			speedx = 10f;
 
-
-		speedy -= 0.1f;
-		if(speedy < -3f)
-			speedy = 3f;
 		tiempo -= Time.deltaTime;
-		transform.Translate (speedx * Time.deltaTime , speedy * Time.deltaTime,speedz * Time.deltaTime);
+		transform.Translate (speedx * Time.deltaTime , 0,speedz * Time.deltaTime);
 	}
 }
--- Assets/Script/PezMovimiento.cs
+++ Assets/Script/PezMovimiento.cs
@@ -2,46 +2,28 @@
 using System.Collections;
 
 public class PezMovimiento : MonoBehaviour {
-	public float speed;
+	public float speed, tiempo = 2;
 	public Transform lago;
-	private NetworkView nw;
 	// Use this for initialization
 	void Start () {
-		int numeroLago = Random.Range (1, 3);
+		int numeroLago = Random.Range (1, 6);
 		string lagoBuscar = "Lago" + numeroLago;
 		lago = GameObject.Find (lagoBuscar).transform;
-		nw = gameObject.GetComponent<NetworkView> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if(nw.isMine)
-		{
-			if(Vector3.Distance(lago.position,transform.position) > 5){
+		if(Vector3.Distance(lago.position,transform.position) > 5 && tiempo < 0){
+			Debug.Log(lago.name);
 				speed = Random.Range (0.1f, 5.0f);
-				int numeroLago = Random.Range (1, 3);
+				int numeroLago = Random.Range (1, 6);
 				string lagoBuscar = "Lago" + numeroLago;
 				lago = GameObject.Find (lagoBuscar).transform;
 				Quaternion rotacion = Quaternion.LookRotation (lago.position - transform.position);
 				transform.rotation = Quaternion.Slerp(transform.rotation, rotacion, speed * Time.deltaTime);
-			}
-			transform.Translate (0,0,speed * Time.deltaTime);
-		}
-
-	}
-
-
-	[RPC]
-	public void pezmovimiento(float speed)
-	{
-		if(Vector3.Distance(lago.position,transform.position) > 5){
-			speed = Random.Range (0.1f, 10.0f);
-			int numeroLago = Random.Range (1, 3);
-			string lagoBuscar = "Lago" + numeroLago;
-			lago = GameObject.Find (lagoBuscar).transform;
-			Quaternion rotacion = Quaternion.LookRotation (lago.position - transform.position);
-			transform.rotation = Quaternion.Slerp(transform.rotation, rotacion, speed * Time.deltaTime);
+			tiempo = 2;
 		}
 		transform.Translate (0,0,speed * Time.deltaTime);
+		tiempo -= Time.deltaTime;
 	}
 }
