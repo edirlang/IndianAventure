8069e20eca74714e2628c32046eee552e0c7181e
Edixon Hernandez <edixon.hernandez.c@gmail.com>
2016-07-21 00:00:31 -0500

    Mejoras de rendimiento

--- Assets/Script/Lago.cs
+++ Assets/Script/Lago.cs
@@ -3,8 +3,6 @@ using System.Collections;
 
 public class Lago : MonoBehaviour {
 
-	float speedx = 1,speedy = 0,speedz = -10, tiempo = 1;
-
 	// Use this for initialization
 	void Start () {
 		transform.Translate (0 , Random.Range(-5,5), Random.Range(-5,5));
@@ -12,16 +10,6 @@ public class Lago : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-		if(tiempo < 0){
-			if(speedz == 10)
-			{	
-				speedz = -10;
-			}else{
-				speedz = 10;
-			}
-		tiempo = 1;
-		}
-		tiempo -= Time.deltaTime;
-		transform.Translate (0 , speedy * Time.deltaTime,speedz * Time.deltaTime);
+			
 	}
 }
--- Assets/Script/PezMovimiento.cs
+++ Assets/Script/PezMovimiento.cs
@@ -2,11 +2,11 @@
 using System.Collections;
 
 public class PezMovimiento : MonoBehaviour {
-	public float speed, tiempo = 2;
+	public float speed, tiempo = 1;
 	public Transform lago;
 	// Use this for initialization
 	void Start () {
-		int numeroLago = Random.Range (1, 6);
+		int numeroLago = Random.Range (1, 3);
 		string lagoBuscar = "Lago" + numeroLago;
 		lago = GameObject.Find (lagoBuscar).transform;
 	}
@@ -14,13 +14,13 @@ public class PezMovimiento : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if(Vector3.Distance(lago.position,transform.position) > 5 && tiempo < 0){
-			speed = Random.Range (0.1f, 5.0f);
+			speed = Random.Range (0.1f, 10.0f);
 			int numeroLago = Random.Range (1, 3);
 			string lagoBuscar = "Lago" + numeroLago;
 			lago = GameObject.Find (lagoBuscar).transform;
 			Quaternion rotacion = Quaternion.LookRotation (lago.position - transform.position);
 			transform.rotation = Quaternion.Slerp(transform.rotation, rotacion, speed * Time.deltaTime);
-			tiempo = 2;
+			tiempo = 1;
 		}
 		transform.Translate (0,0,speed * Time.deltaTime);
 		tiempo -= Time.deltaTime;
